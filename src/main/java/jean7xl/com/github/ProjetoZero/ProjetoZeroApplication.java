71a249c77207eb452e4ef36fa0253ed2d35eb661
Jean lucas <jeanlctt@gmail.com>
2026-02-04 20:55:29 -0300

    first commit

---
+++ src/main/java/jean7xl/com/github/ProjetoZero/ProjetoZeroApplication.java
@@ -0,0 +1,13 @@
+package jean7xl.com.github.ProjetoZero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoZeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jean7xl/com/github/ProjetoZero/ProjetoZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package jean7xl.com.github.ProjetoZero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
