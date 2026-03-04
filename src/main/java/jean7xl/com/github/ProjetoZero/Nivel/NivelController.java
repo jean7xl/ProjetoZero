7ae8d6cf89ba70f57fde9974f3b6a4b5908cd135
Jean lucas <jeanlctt@gmail.com>
2026-03-04 11:33:53 -0300

    fix- ALtera o nome das rotas no controler

--- src/main/java/jean7xl/com/github/ProjetoZero/Nivel/NivelController.java
+++ src/main/java/jean7xl/com/github/ProjetoZero/Nivel/NivelController.java
@@ -1,10 +1,28 @@
 package jean7xl.com.github.ProjetoZero.Nivel;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/nivel")
 public class NivelController {
+
+    @GetMapping("/mostrarTodos")
+    public String mostrarTodos(){
+        return "Mostrar Todas os niveis";
+    }
+
+    @DeleteMapping("/deleteID")
+    public String deletarPorID(){
+        return "Deletar por ID";
+    }
+    @PutMapping("/atualizaId")
+    public String atualizaPorID(){
+        return "Atualiza por ID";
+    }
+
+    @PostMapping("/criarnivel")
+    String criarNivel(){
+        return "Cria um nivel";
+    }
 
 }
