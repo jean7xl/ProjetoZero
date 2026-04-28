dfb5c5dddbe33fd8d492ab4ed85bce4ba2a7c806
Jean lucas <jeanlctt@gmail.com>
2026-04-28 14:42:34 -0300

    chore- inclui a verssao do DTO pela entindade e mappea manualmente no UsuarioMapper

--- src/main/java/jean7xl/com/github/ProjetoZero/Usuario/UsuarioDTO.java
+++ src/main/java/jean7xl/com/github/ProjetoZero/Usuario/UsuarioDTO.java
@@ -1,4 +1,26 @@
 package jean7xl.com.github.ProjetoZero.Usuario;
 
+import jakarta.persistence.*;
+import jean7xl.com.github.ProjetoZero.Nivel.NivelModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
+
+
+    private Long id;
+    private String nome;
+    private String imgUrl;
+    private String email;
+    private int idade;
+    private  String rank ;
+    private NivelModel nivelModel;
+
+    //
 }
+
+
--- src/main/java/jean7xl/com/github/ProjetoZero/Usuario/UsuarioMapper.java
+++ src/main/java/jean7xl/com/github/ProjetoZero/Usuario/UsuarioMapper.java
@@ -1,4 +1,33 @@
 package jean7xl.com.github.ProjetoZero.Usuario;
 
+import jean7xl.com.github.ProjetoZero.Nivel.NivelModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UsuarioMapper {
+
+    public NinjaModel map(UsuarioDTO usuarioDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+
+        ninjaModel.setId(usuarioDTO.getId());
+        ninjaModel.setNome(usuarioDTO.getNome());
+        ninjaModel.setEmail(usuarioDTO.getEmail());
+        ninjaModel.setImgUrl(usuarioDTO.getImgUrl());
+        ninjaModel.setRank(usuarioDTO.getRank());
+
+        return ninjaModel;
+
+    }
+    public UsuarioDTO map(NinjaModel ninjaModel) {
+
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+
+        usuarioDTO.setId(ninjaModel.getId());
+        usuarioDTO.setNome(ninjaModel.getNome());
+        usuarioDTO.setEmail(ninjaModel.getEmail());
+        usuarioDTO.setImgUrl(ninjaModel.getImgUrl());
+        usuarioDTO.setRank(ninjaModel.getRank());
+        return usuarioDTO;
+
+    }
 }
